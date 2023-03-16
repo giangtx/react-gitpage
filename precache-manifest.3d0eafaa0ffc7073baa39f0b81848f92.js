db41862656337c0ccba8b162d2492b9c557e3d61
unknown <giangtx13@gmail.com>
2023-03-16 17:20:31 +0700

    Updates

---
+++ precache-manifest.3d0eafaa0ffc7073baa39f0b81848f92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8df7cf6503bd33a563c",
+    "url": "/react-gitpage/static/css/main.7aaab21a.chunk.css"
+  },
+  {
+    "revision": "e8df7cf6503bd33a563c",
+    "url": "/react-gitpage/static/js/main.a818ab06.chunk.js"
+  },
+  {
+    "revision": "f28b4d24676f978cd1b4",
+    "url": "/react-gitpage/static/js/runtime~main.9e45204d.js"
+  },
+  {
+    "revision": "8cc2edafe47b3b0b77da",
+    "url": "/react-gitpage/static/js/2.a20e9c33.chunk.js"
+  },
+  {
+    "revision": "83136dc78a7a647170c784be4ea0dfb9",
+    "url": "/react-gitpage/index.html"
+  }
+];
