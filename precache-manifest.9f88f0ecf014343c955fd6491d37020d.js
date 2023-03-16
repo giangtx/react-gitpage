15ff3328f593f5141bbe57f639022aac4aa3c88d
unknown <giangtx13@gmail.com>
2023-03-16 18:42:54 +0700

    Updates

---
+++ precache-manifest.9f88f0ecf014343c955fd6491d37020d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "202afc4c4a88f086afb0",
+    "url": "/static/css/main.9f805d5f.chunk.css"
+  },
+  {
+    "revision": "202afc4c4a88f086afb0",
+    "url": "/static/js/main.1c9ce898.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "545aa8919a772e1044ba",
+    "url": "/static/js/2.3cb5920b.chunk.js"
+  },
+  {
+    "revision": "41f5e8ff1d98d490a19c6d48ea7b74b1",
+    "url": "/static/media/beyond_the_mountains.41f5e8ff.ttf"
+  },
+  {
+    "revision": "716ac90b0e030c14bafa4c95f53fe13b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gitpage/precache-manifest.893745bb293ad104e2703e8b7a5ddb11.js"
+  "/precache-manifest.9f88f0ecf014343c955fd6491d37020d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gitpage/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
